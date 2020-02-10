dfab592366aa3ef4737bbaf8e41e5fb19116545e
Hashed <z-donnelly@hvcc.edu>
2020-02-09 20:10:28 -0500

    set up basic TypeScript config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,10 @@
 module.exports = {
-  addons: [
-    "@storybook/preset-create-react-app",
-    "@storybook/addon-knobs/register",
-    "@storybook/addon-links/register",
-    "@storybook/addon-actions/register",
-    "@storybook/addon-a11y/register"
-  ],
-  stories: ["../src/**/*.stories.[tj]s"]
+	addons: [
+		'@storybook/preset-create-react-app',
+		'@storybook/addon-knobs/register',
+		'@storybook/addon-links/register',
+		'@storybook/addon-actions/register',
+		'@storybook/addon-a11y/register',
+	],
+	stories: ['../src/**/*.stories.[tj]sx'],
 };
