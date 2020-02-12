13def29d414c36e501843bb1ca9c2f58447dc408
Hashed <z-donnelly@hvcc.edu>
2020-02-12 03:29:02 -0500

    finish sidebar, add all sidebar images, add favicon

--- src/components/dashboard/dashboard.tsx
+++ src/components/dashboard/dashboard.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../navbar/navbar';
+import Sidebar from '../sidebar/sidebar';
 
 const Dashboard = () => {
 	return (
 		<div>
 			<Navbar />
+			<Sidebar />
 		</div>
 	);
 };
